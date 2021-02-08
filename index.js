9fe320c7f126c529996d0edb72b197aac16d89a9
talryan <nataliamryan@gmail.com>
2021-02-08 09:00:28 -0800

    commit tree 4455b03956d530a6cca19d1817e1d7b34ee28f19

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+  let win = array.find( ({result}) => result === "W")
+   if (win){
+      return win.year
+     }else {
+       return undefined
+      }
+
+}
